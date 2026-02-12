b3505985506415eabfaed4fd1eeb125dbcc5f48a
OUSSAMA <oussamacharef71@gmail.com>
2026-02-12 16:03:58 +0100

    add chapiters 6_7

---
+++ les_chapiter/chapiter_7/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon Site PHP</title>
+</head>
+<body>
+
+    <header>
+
+            <h1>Binvenue sur mon site</h1>
+
+            <hr>
+    </header>
+    
+</body>
+</html>
---
+++ les_chapiter/chapiter_7/index.php
@@ -0,0 +1,26 @@
+<?php
+
+include 'header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon Site PHP</title>
+</head>
+<body>
+
+<main>
+    <h2>Page d'accueil</h2>
+    <p>Contenu de la page principale.</p>
+</main>
+    
+</body>
+</html>
+
+<?php
+include 'footer.php';
+
+?>
